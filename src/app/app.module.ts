269fa4d42ac1d9a87b1e4bc44e4d823fd328a990
Sam Zagrobelny <strangesast@gmail.com>
2017-05-25 17:46:30 -0400

    partial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { BaseComponent } from './base/base.component';
+
+import { MapdataService } from './mapdata.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: BaseComponent,
+        resolve: {
+          mapdata: MapdataService
+        }
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    MapdataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
